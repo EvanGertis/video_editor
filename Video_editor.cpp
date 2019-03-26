fd62d06abb1163d3ad350a109207b35a3df80aa9
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-03-26 11:44:44 -0400

    opencv added

--- Video_editor.cpp
+++ Video_editor.cpp
@@ -1,21 +1,19 @@
-// Video_editor.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+File Name: Video_editor.cpp.
+Author: Evan Gertis.
+Date: 03/26/2019.
+Purpose: Main entry point for my video editing program.
+Credit: opencv opensource project.
+License: None.
+*/
 
 #include "pch.h"
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

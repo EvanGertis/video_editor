3ae325ea6bb74e411ba7b1960b09981534a643f1
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-03-26 11:53:27 -0400

    added capture loop

--- Video_editor.cpp
+++ Video_editor.cpp
@@ -13,7 +13,39 @@ License: None.
 
 int main()
 {
-    
+	//open the camera.
+	cv::VideoCapture cap(0);
+	
+	//guard: if the camera isn't available -> exit.
+	if (!cap.isOpened()) {
+		std::cout << "Unable to open camera." << std::endl;
+		exit(-1);
+	}
+
+	//storage for the captured frame.
+	cv::Mat image;
+	double FPS = 24.0; //set the frame rate.
+	
+	//camera capture loop.
+	while (true) {
+		cap >> image; // write the captured frame.
+
+		//guard: if the frame is empty => throw error.
+		if (image.empty()) {
+			std::cout << "cannot read frame";
+			break; //exit loop.
+		}
+
+		//show the video.
+		cv::imshow("My video", image);
+
+		//break on ESC key.
+		if (cv::waitKey(1000.0 / FPS) == 27) {
+			break; //exits the loop.
+		}
+
+	}//end capture loop.
+	
 }
 
 
